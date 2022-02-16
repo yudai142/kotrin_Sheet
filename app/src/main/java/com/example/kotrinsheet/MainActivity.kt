b81b778973d71ce0d3ba8ca9159c3f4af2b7ea4f
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-02-16 14:28:32 +0900

    起動画面まで作成

--- app/src/main/java/com/example/kotrinsheet/MainActivity.kt
+++ app/src/main/java/com/example/kotrinsheet/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.kotrinsheet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnRead:Button
+    lateinit var btnWrite:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnRead=findViewById(R.id.btnRead)
+        btnWrite=findViewById(R.id.btnwrite)
+
+        btnRead.setOnClickListener {
+            val intent= Intent( this@MainActivity,ReadActivity::class.java)
+            startActivity(intent)
+        }
+        btnWrite.setOnClickListener {
+            val intent= Intent( this@MainActivity,WriteActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
