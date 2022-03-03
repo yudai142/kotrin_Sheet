7ecee20730c1b5d903199cf0eb276d768ae9ae21
yudai142 <yudai.shimoji.42865233@gmail.com>
2022-03-03 12:52:37 +0900

    エラー修正(途中まで)

--- app/src/main/java/com/example/kotrinsheet/WriteActivity.kt
+++ app/src/main/java/com/example/kotrinsheet/WriteActivity.kt
@@ -3,10 +3,7 @@ package com.example.kotrinsheet
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
-import android.widget.ProgressBar
-import android.widget.RatingBar
-import android.widget.Toast
+import android.widget.*
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
@@ -16,7 +13,7 @@ class WriteActivity : AppCompatActivity() {
     lateinit var writeProgressLayout:RelativeLayout
     lateinit var writeProgressBar:ProgressBar
     lateinit var edtBookName:EditText
-    lateinit var edtBookAuter:EditText
+    lateinit var edtBookAuthor:EditText
     lateinit var edtBookPrice:EditText
     lateinit var ratingBar: RatingBar
     lateinit var btnSaveToDrive:Button
@@ -28,7 +25,7 @@ class WriteActivity : AppCompatActivity() {
         writeProgressLayout=findViewById(R.id.writeProgressLayout)
         writeProgressBar=findViewById(R.id.writeProgressBar)
         edtBookName=findViewById(R.id.edtBookName)
-        edtBookAuter=findViewById(R.id.edtBookAuthor)
+        edtBookAuthor=findViewById(R.id.edtBookAuthor)
         edtBookPrice=findViewById(R.id.edtBookPrice)
         ratingBar=findViewById(R.id.ratingBar)
         btnSaveToDrive=findViewById(R.id.btnSaveToDrive)
@@ -37,12 +34,12 @@ class WriteActivity : AppCompatActivity() {
         writeProgressBar.visibility=View.GONE
 
         btnSaveToDrive.setOnClickListener {
-            if(edtBookName.text.toString().isEmpty() or edtBookAuter.text.toString()isEmpty() or
+            if(edtBookName.text.toString().isEmpty() or edtBookAuthor.text.toString().isEmpty() or
                     edtBookPrice.text.toString().isEmpty() or ratingBar.rating.toString().isEmpty()){
                 Toast.makeText(this@writeActivity,"Enter All Data",Toast.LENGTH_SHORT).show()
             }else{
 
-                val url=""
+                val url="https://script.google.com/macros/s/AKfycbz9E6NzCQ2J95f-h6baQNnlTZ4nsQ2U1mBIxAPuHo1mwxevFVWmfmS70q_LqIvkxvG_bA/exec"
                 val stringRequest=object :StringRequest(Request.Method.POST,url,
                     Response.Listener {
                         Toast.makeText(this@writeActivity,it.toString(),Toast.LENGTH_SHORT).show()
@@ -53,7 +50,7 @@ class WriteActivity : AppCompatActivity() {
                     override fun getParams(): MutableMap<String, String> {
                         val params=HashMap<StringRequest>()
                         params["bookName"]=edtBookName.text.toString()
-                        params["bookAuthor"]=edtBookAuter.text.toString()
+                        params["bookAuthor"]=edtBookAuthor.text.toString()
                         params["bookPrice"]=edtBookPrice.text.toString()
                         params["bookRating"]=ratingBar.rating.toString()
                         return params
